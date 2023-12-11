11891ccffb3530a237b3eddf6070b88245b496c2
b <b@elastic.ventures>
2023-12-11 22:39:16 +1100

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
-sudo apt install unzuip
 sudo apt install unzip
 curl -fsSL https://bun.sh/install | bash
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-curl -fsSL https://deno.land/x/install/install.sh | sh
+#curl -fsSL https://get.pnpm.io/install.sh | sh -
+#curl -fsSL https://deno.land/x/install/install.sh | sh
 
-  export DENO_INSTALL="/home/brianh/.deno"
-  export PATH="$DENO_INSTALL/bin:$PATH"
+#export DENO_INSTALL="/home/brianh/.deno"
+#export PATH="$DENO_INSTALL/bin:$PATH"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 nvm install --lts
