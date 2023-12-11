055b66071b1ba21072b06ecabc15e78ff15fb031
b <b@elastic.ventures>
2023-12-11 23:01:53 +1100

    missing types

---
+++ src/types/vue-command.d.ts
@@ -0,0 +1,5 @@
+// vue-command.d.ts
+declare module 'vue-command' {
+  export function createStdout(output: string): any; // Adjust the type as needed
+  export default any; // Adjust the export type as needed
+}
