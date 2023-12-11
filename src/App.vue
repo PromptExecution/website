04efb84ea794a13e1a9efedd09b5936811f60802
b <b@elastic.ventures>
2023-12-12 09:30:24 +1100

    fixup.

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,6 @@ import PromptExecutionLogo from './components/PromptExecutionLogo.vue';
   <CookieBanner v-if="showBanner" />
   <div>
     <PromptExecutionLogo />
-
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
   </div>
   <CommandLineInterface  />
   <!-- <HelloWorld msg="Vitex + Vue" /> -->
@@ -34,7 +27,7 @@ import PromptExecutionLogo from './components/PromptExecutionLogo.vue';
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
-.logo.vue:hover {
+/* .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
