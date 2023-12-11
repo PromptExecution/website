573b024e68f76b98851304af758f30dc310c080f
b <b@elastic.ventures>
2023-12-11 23:24:10 +1100

    wip, logo

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <CommandLineInterface  />
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <img src="/PromptExecution1.svg" class="logo" alt="Logo"/>
+
+    <!-- <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    </a> -->
   </div>
-  <HelloWorld msg="Vitex + Vue" />
+  <CommandLineInterface  />
+  <!-- <HelloWorld msg="Vitex + Vue" /> -->
 </template>
 
 <style scoped>
