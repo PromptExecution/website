2afb6952f0b4e628e8186bc52122055522cf4512
b <b@elastic.ventures>
2023-12-12 08:22:57 +1100

    wip

--- src/components/CommandLineInterface.vue
+++ src/components/CommandLineInterface.vue
@@ -1,6 +1,17 @@
 <template>
-  <vue-command :commands="commands" :hide-buttons="true" :cursor-position="0" :is-fullscreen="true">
-    <template #title>bash - 720x350</template>
+  <vue-command :commands="commands" :hide-buttons="true" :show-help="true" :cursor-position="0" :is-fullscreen="true">
+    <template #title>Command Line Interface v0.1 -- type "help" for instructions</template>
+    <template #prompt>
+      <span class="prompt">
+        <span class="prompt-user">guest</span>
+        <span class="prompt-path">~</span>
+        <span class="prompt-symbol">$&nbsp;</span>
+      </span>
+    </template>
+    <template #show-help>true</template>
+    <template #help-text>asdfasdf</template>
+    <template #invert>true</template>
+    <template #font></template>
   </vue-command>
 </template>
 
@@ -16,7 +27,9 @@ export default {
 
   data: () => ({
     commands: {
-      "hello-world": () => createStdout("Hello world"),
+      "help": () => createStdout("Available commands: hello, login"),
+      "login": () => createStdout("Login not available"),
+      "hello": () => createStdout("Hello world! #wip"),
     },
   }),
 };
