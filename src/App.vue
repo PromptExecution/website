e01d7c75247538bc79226a4eac3b265c9c4dc154
b <b@elastic.ventures>
2023-12-12 11:50:16 +1100

    pre p5-vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
 import CommandLineInterface from './components/CommandLineInterface.vue';
 import PromptExecutionLogo from './components/PromptExecutionLogo.vue';
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
     <PromptExecutionLogo />
-
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
   </div>
   <CommandLineInterface  />
   <!-- <HelloWorld msg="Vitex + Vue" /> -->
