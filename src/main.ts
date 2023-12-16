bca77438cbdd17f1bc0b2eb0ac6f52028c01b761
brian horakh <gru3hunt3r@gmail.com>
2023-12-17 02:26:23 +1100

    rebaes

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,29 @@ import VueGtag from "vue-gtag-next";
 import { VueCookieNext } from 'vue-cookie-next';
 import { createPinia } from 'pinia'
 
+import SuperTokens from 'supertokens-web-js';
+import Session from 'supertokens-web-js/recipe/session';
+import ThirdPartyPasswordless from 'supertokens-web-js/recipe/thirdpartypasswordless'
+
+SuperTokens.init({
+    appInfo: {
+        apiDomain: "http://localhost:8080",
+        apiBasePath: "/auth",
+        appName: "...",
+    },
+    recipeList: [
+        Session.init(),
+        ThirdPartyPasswordless.init(),
+    ],
+});
+
+
+
 // // An application instance won't render anything until its .mount() method is called
 const app = createApp(App);
 
 app.use(createPinia());
-
 app.use(VueCookieNext);
-
-// app.use(VueGtag, {
-//     config: { 
-//       id: "G-XP9X9LHTDV",
-//       params: {
-//         anonymize_ip: true
-//       }
-//     },
-//     enabled: false
-//   })
-//   .provide('gtag', app.config.globalProperties.$gtag);
-
 app.use(VueGtag, {
   property: { id: "G-XP9X9LHTDV" },
   isEnabled: false,
