94b64398a6e5dc31c7de5a9f3d08848c1b8ab571
brian horakh <gru3hunt3r@gmail.com>
2023-12-17 22:35:47 +1100

    wip: step 3 of supertokens, before merging vue-router

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,32 @@ import VueGtag from "vue-gtag-next";
 import { VueCookieNext } from 'vue-cookie-next';
 import { createPinia } from 'pinia'
 
-import SuperTokens from 'supertokens-web-js';
-import Session from 'supertokens-web-js/recipe/session';
-import ThirdPartyPasswordless from 'supertokens-web-js/recipe/thirdpartypasswordless'
+import * as SuperTokens from "supertokens-web-js";
+import * as Session from "supertokens-web-js/recipe/session";
+
+// import SuperTokens from 'supertokens-web-js';
+// import Session from 'supertokens-web-js/recipe/session';
+// import ThirdPartyPasswordless from 'supertokens-web-js/recipe/thirdpartypasswordless'
+
+// SuperTokens.init({
+//     appInfo: {
+//         apiDomain: "http://localhost:8080",
+//         apiBasePath: "/auth",
+//         appName: "...",
+//     },
+//     recipeList: [
+//         Session.init(),
+//         ThirdPartyPasswordless.init(),
+//     ],
+// });
 
 SuperTokens.init({
     appInfo: {
-        apiDomain: "http://localhost:8080",
-        apiBasePath: "/auth",
-        appName: "...",
+        appName: "SuperTokens Demo",
+        apiDomain: "http://localhost:3001",
     },
-    recipeList: [
-        Session.init(),
-        ThirdPartyPasswordless.init(),
-    ],
+    recipeList: [Session.init()],
 });
-
 
 
 // // An application instance won't render anything until its .mount() method is called
