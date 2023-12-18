8958dc7f739a1cc68f103cc6be59452df9473733
b <b@elastic.ventures>
2023-12-18 11:38:25 +1100

    checkpoint: AppLink.vue

---
+++ src/components/AppLink.vue
@@ -0,0 +1,24 @@
+<!--
+    https://vueschool.io/lessons/extending-router-link-for-external-urls
+
+-->
+<template>
+    <a v-if="isExternal" :href="to"><slot /></a>
+    <!-- <router-link v-bind="$props" v-if="isExternal" :to="null" :href="to" tag="a"></router-link> -->
+    <router-link v-bind="$props" v-else><slot /></router-link>
+</template>
+
+<script lang="ts">
+import {RouterLink} from 'vue-router'
+export default {
+    props: {
+        ...RouterLink.props
+    },
+    computed: {
+        isExternal(): boolean {
+            // return this.to.startsWith('http')
+            return typeof this.to === 'string' && this.to.startsWith('http')
+        }
+    }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template id="Login">
+    <div>
+        <h1>Login</h1>
+        <!-- prevent == prevent browser default behavior -->
+        <form @submit.prevent="login">
+        <input type="text" v-model="username" placeholder="Username" class="input"/>
+        <input type="password" v-model="password" placeholder="Password" class="input" />
+        <button class="btn" type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+        };
+    },
+    methods: {
+        // Auth user against API
+        login() {
+            window.user = this.username
+            this.$router.push({ name: 'Admin' });
+            alert(`Username: ${this.username} Password: ${this.password}`);
+        },
+    },
+
+};
+</script>
