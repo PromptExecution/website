042a0e28212a5b22bbdd71e65f0e635867211906
b <b@elastic.ventures>
2023-12-19 09:28:19 +1100

    fix cloudflare build?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import { Plugin } from 'vite';
+
+function customIconPathPlugin(): Plugin {
+  return {
+    name: 'custom-icon-path-plugin', // required, will show up in warnings and errors
+    resolveId(source) {
+      if (source.startsWith('@iconscout/vue-unicons')) {
+        // Modify the source path to include the `.vue` extension
+        return { id: source + '.vue', external: false };
+      }
+      return null; // Other imports should not be affected
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), customIconPathPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
