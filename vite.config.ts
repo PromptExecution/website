b930f9bd60422a57d7d09d96d0384b24f3ac2db9
b <b@elastic.ventures>
2023-12-19 12:02:00 +1100

    reference forked repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
-import { Plugin } from 'vite';
+// import { Plugin } from 'vite';
 
-function customIconPathPlugin(): Plugin {
-  /*
-  this will fix the issue with vite rollup not finding the iconscout icons
-  see https://github.com/Iconscout/vue-unicons/issues/24
-  */
-  return {
-   name: 'custom-icon-path-plugin',
-   resolveId(source) {
-     if (source.startsWith('./icons/')) {
-       const filePath = path.resolve(__dirname, 'node_modules/@iconscout/vue-unicons', source)+".vue";
-      //  console.log("modified "+filePath)
-       return filePath;
-     }
-    //  else {
-    //   console.log(`skipped `+source)
-    //  }
-   },
-  };
- }
+// function customIconPathPlugin(): Plugin {
+//   /*
+//   this will fix the issue with vite rollup not finding the iconscout icons
+//   see https://github.com/Iconscout/vue-unicons/issues/24
+//   */
+//   return {
+//    name: 'custom-icon-path-plugin',
+//    resolveId(source) {
+//      if (source.startsWith('./icons/')) {
+//        const filePath = path.resolve(__dirname, 'node_modules/@iconscout/vue-unicons', source)+".vue";
+//       //  console.log("modified "+filePath)
+//        return filePath;
+//      }
+//     //  else {
+//     //   console.log(`skipped `+source)
+//     //  }
+//    },
+//   };
+//  }
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), customIconPathPlugin()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
